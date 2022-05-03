46a36f3e8a1cbdbc21dd5b7c6a999370af4cc6cb
Ilias Garnier <ilias.garnier@nomadic-labs.com>
2022-05-03 08:59:15 +0200

    WIP

---
+++ src/lib_context/disk/tezos_context.ml
@@ -0,0 +1,277 @@
+(*****************************************************************************)
+(*                                                                           *)
+(* Open Source License                                                       *)
+(* Copyright (c) 2022-2022 Tarides <contact@tarides.com>                     *)
+(*                                                                           *)
+(* Permission is hereby granted, free of charge, to any person obtaining a   *)
+(* copy of this software and associated documentation files (the "Software"),*)
+(* to deal in the Software without restriction, including without limitation *)
+(* the rights to use, copy, modify, merge, publish, distribute, sublicense,  *)
+(* and/or sell copies of the Software, and to permit persons to whom the     *)
+(* Software is furnished to do so, subject to the following conditions:      *)
+(*                                                                           *)
+(* The above copyright notice and this permission notice shall be included   *)
+(* in all copies or substantial portions of the Software.                    *)
+(*                                                                           *)
+(* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR*)
+(* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,  *)
+(* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL   *)
+(* THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER*)
+(* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING   *)
+(* FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER       *)
+(* DEALINGS IN THE SOFTWARE.                                                 *)
+(*                                                                           *)
+(*****************************************************************************)
+
+module type S = sig
+  (** {2 Generic interface} *)
+
+  type error +=
+    | Cannot_create_file of string
+    | Cannot_open_file of string
+    | Cannot_find_protocol
+    | Suspicious_file of int
+
+  module type S = sig
+    (** @inline *)
+    include Tezos_context_sigs.Context.S
+  end
+
+  (** A block-indexed (key x value) store directory.  *)
+  type index
+
+  include S with type index := index
+
+  type context = t
+
+  val index : context -> index
+
+  (** Open or initialize a versioned store at a given path.
+
+      @param indexing_strategy determines whether newly-exported objects by
+      this store handle should also be added to the store's index. [`Minimal]
+      (the default) only adds objects to the index when they are {i commits},
+      whereas [`Always] indexes every object type. The indexing strategy used
+      for existing stores can be changed without issue (as only {i
+      newly}-exported objects are impacted). *)
+  val init :
+    ?patch_context:(context -> context tzresult Lwt.t) ->
+    ?readonly:bool ->
+    ?indexing_strategy:[`Always | `Minimal] ->
+    ?index_log_size:int ->
+    string ->
+    index Lwt.t
+
+  (** Close the index. Does not fail when the context is already closed. *)
+  val close : index -> unit Lwt.t
+
+  (** Sync the context with disk. Only useful for read-only instances.
+    Does not fail when the context is not in read-only mode. *)
+  val sync : index -> unit Lwt.t
+
+  val flush : t -> t Lwt.t
+
+  val compute_testchain_chain_id : Block_hash.t -> Chain_id.t
+
+  val compute_testchain_genesis : Block_hash.t -> Block_hash.t
+
+  (** Build an empty context from an index. The resulting context should not
+      be committed. *)
+  val empty : index -> t
+
+  (** Returns [true] if the context is empty. *)
+  val is_empty : t -> bool
+
+  val commit_genesis :
+    index ->
+    chain_id:Chain_id.t ->
+    time:Time.Protocol.t ->
+    protocol:Protocol_hash.t ->
+    Context_hash.t tzresult Lwt.t
+
+  val commit_test_chain_genesis :
+    context -> Block_header.t -> Block_header.t Lwt.t
+
+  (** Extract a subtree from the {!t} argument and returns
+      it as a {!Tezos_context_memory.Disk_context.tree} (note the the type change!). **)
+  val to_memory_tree :
+    t -> string list -> Tezos_context_memory.Context.tree option Lwt.t
+
+  (** [merkle_tree t leaf_kind key] returns a Merkle proof for [key] (i.e.
+    whose hashes reach [key]). If [leaf_kind] is [Block_services.Hole], the value
+    at [key] is a hash. If [leaf_kind] is [Block_services.Raw_context],
+    the value at [key] is a [Block_services.raw_context]. Values higher
+    in the returned tree are hashes of the siblings on the path to
+    reach [key]. *)
+  val merkle_tree :
+    t ->
+    Block_services.merkle_leaf_kind ->
+    key ->
+    Block_services.merkle_tree Lwt.t
+
+  (** {2 Accessing and Updating Versions} *)
+
+  val exists : index -> Context_hash.t -> bool Lwt.t
+
+  val checkout : index -> Context_hash.t -> context option Lwt.t
+
+  val checkout_exn : index -> Context_hash.t -> context Lwt.t
+
+  val hash : time:Time.Protocol.t -> ?message:string -> t -> Context_hash.t
+
+  val commit :
+    time:Time.Protocol.t -> ?message:string -> context -> Context_hash.t Lwt.t
+
+  val set_head : index -> Chain_id.t -> Context_hash.t -> unit Lwt.t
+
+  val set_master : index -> Context_hash.t -> unit Lwt.t
+
+  (** {2 Hash version} *)
+
+  (** Get the hash version used for the context *)
+  val get_hash_version : context -> Context_hash.Version.t
+
+  (** Set the hash version used for the context.  It may recalculate the hashes
+    of the whole context, which can be a long process.
+    Returns an [Error] if the hash version is unsupported. *)
+  val set_hash_version :
+    context -> Context_hash.Version.t -> context tzresult Lwt.t
+
+  (** {2 Predefined Fields} *)
+
+  val get_protocol : context -> Protocol_hash.t Lwt.t
+
+  val add_protocol : context -> Protocol_hash.t -> context Lwt.t
+
+  val get_test_chain : context -> Test_chain_status.t Lwt.t
+
+  val add_test_chain : context -> Test_chain_status.t -> context Lwt.t
+
+  val remove_test_chain : context -> context Lwt.t
+
+  val fork_test_chain :
+    context ->
+    protocol:Protocol_hash.t ->
+    expiration:Time.Protocol.t ->
+    context Lwt.t
+
+  val clear_test_chain : index -> Chain_id.t -> unit Lwt.t
+
+  val find_predecessor_block_metadata_hash :
+    context -> Block_metadata_hash.t option Lwt.t
+
+  val add_predecessor_block_metadata_hash :
+    context -> Block_metadata_hash.t -> context Lwt.t
+
+  val find_predecessor_ops_metadata_hash :
+    context -> Operation_metadata_list_list_hash.t option Lwt.t
+
+  val add_predecessor_ops_metadata_hash :
+    context -> Operation_metadata_list_list_hash.t -> context Lwt.t
+
+  (** {2 Context dumping} *)
+
+  val dump_context :
+    index ->
+    Context_hash.t ->
+    fd:Lwt_unix.file_descr ->
+    on_disk:bool ->
+    progress_display_mode:Animation.progress_display_mode ->
+    int tzresult Lwt.t
+
+  (** Rebuild a context from a given snapshot. *)
+  val restore_context :
+    index ->
+    expected_context_hash:Context_hash.t ->
+    nb_context_elements:int ->
+    fd:Lwt_unix.file_descr ->
+    legacy:bool ->
+    in_memory:bool ->
+    progress_display_mode:Animation.progress_display_mode ->
+    unit tzresult Lwt.t
+
+  val retrieve_commit_info :
+    index ->
+    Block_header.t ->
+    (Protocol_hash.t
+    * string
+    * string
+    * Time.Protocol.t
+    * Test_chain_status.t
+    * Context_hash.t
+    * Block_metadata_hash.t option
+    * Operation_metadata_list_list_hash.t option
+    * Context_hash.t list)
+    tzresult
+    Lwt.t
+
+  val check_protocol_commit_consistency :
+    expected_context_hash:Context_hash.t ->
+    given_protocol_hash:Protocol_hash.t ->
+    author:string ->
+    message:string ->
+    timestamp:Time.Protocol.t ->
+    test_chain_status:Test_chain_status.t ->
+    predecessor_block_metadata_hash:Block_metadata_hash.t option ->
+    predecessor_ops_metadata_hash:Operation_metadata_list_list_hash.t option ->
+    data_merkle_root:Context_hash.t ->
+    parents_contexts:Context_hash.t list ->
+    bool Lwt.t
+
+  (** Offline integrity checking and statistics for contexts. *)
+  module Checks : sig
+    module Pack : Irmin_pack_unix.Checks.S
+
+    module Index : Index.Checks.S
+  end
+end
+
+(** Context comes with two variants: [Context] and [Context_binary] with
+    different tradeoffs.
+
+    Both have different Merkle tree representations (i.e. when presented the
+    same data, they don't produce the same hashes).
+
+    [lib_context] represents directories as a structured tree of inodes, instead
+    of a flat list of files, to get efficient copy-on-write and optimised read
+    patterns.
+
+    The context variants differ by the branching factors used for these inode
+    trees:
+
+    - [Context] uses a branching factor of 32;
+    - [Context_binary] uses a branching factor of 2.
+
+    To represent a large directory, [Context] uses less but larger inodes than
+    [Context_binary].
+
+    As persisting inodes on disk have an overhead (i.e. the serialisation of an
+    inode is prefixed by its 32 byte hash), [Context] is thus optimised for
+    storing a large quantity of data on disk.
+
+    On the opposite, as the inodes in Merkle proofs contain the hashes of the
+    shallow siblings, [Context_binary] is thus optimised for producing smaller
+    Merkle proofs. *)
+
+module Context_binary = struct
+  type error +=
+    | Cannot_create_file = Disk_context.Cannot_create_file
+    | Cannot_open_file = Disk_context.Cannot_open_file
+    | Cannot_find_protocol = Disk_context.Cannot_find_protocol
+    | Suspicious_file = Disk_context.Suspicious_file
+
+  include Disk_context.Make (Tezos_context_encoding.Context_binary)
+end
+
+(** The context of a tezos node. Persisted to disk. *)
+module Context = struct
+  type error +=
+    | Cannot_create_file = Disk_context.Cannot_create_file
+    | Cannot_open_file = Disk_context.Cannot_open_file
+    | Cannot_find_protocol = Disk_context.Cannot_find_protocol
+    | Suspicious_file = Disk_context.Suspicious_file
+
+  include Disk_context.Make (Tezos_context_encoding.Context)
+end
+
+module Context_dump = Context_dump
